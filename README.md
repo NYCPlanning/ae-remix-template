0e0006905656965ab8f083cce07403ca895aa3ef
tangoyankee <miller.tim108@gmail.com>
2024-02-27 16:30:18 -0500

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Description
+
+Template for Remix applications with Deck.gl and @nycplanning/streetscape (based on Chakra UI)
+
+## Dev setup
+- `nvm use`
+- `npm i`
+- `npm run dev`
---
+++ app/root.tsx
@@ -0,0 +1,74 @@
+import { StreetscapeProvider, Box, Heading } from "@nycplanning/streetscape";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
+} from "@remix-run/react";
+
+function Document({
+  children,
+  title = "App title",
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <title>{title}</title>
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <StreetscapeProvider>
+        <Outlet />
+      </StreetscapeProvider>
+    </Document>
+  );
+}
+
+// How StreetscapeProvider should be used on ErrorBoundary
+export function ErrorBoundary() {
+  const error = useRouteError();
+  if (isRouteErrorResponse(error)) {
+    return (
+      <Document>
+        <StreetscapeProvider>
+          <Box>
+            <Heading as="h1" bg="purple.600">
+              [CatchBoundary]: {error.status} {error.statusText}
+            </Heading>
+          </Box>
+        </StreetscapeProvider>
+      </Document>
+    );
+  }
+
+  const errorMessage = error instanceof Error ? error.message : "unknown error";
+  return (
+    <Document title="Error!">
+      <StreetscapeProvider>
+        <Box>
+          <Heading as="h1" bg="blue.500">
+            [ErrorBoundary]: There was an error: {errorMessage}
+          </Heading>
+        </Box>
+      </StreetscapeProvider>
+    </Document>
+  );
+}
