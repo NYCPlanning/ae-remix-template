b696015cdb8d0eb4ed44143586b57936b198620f
tangoyankee <miller.tim108@gmail.com>
2024-03-04 14:42:44 -0500

    configure Vitest, RTL, and Kubb
    Configure vitest
    Install React testing library
    Install kubb to generate typescript, zod, axios, and mock service worker with faker

---
+++ kubb.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from "@kubb/core";
+import createSwagger from "@kubb/swagger";
+import createSwaggerTS from "@kubb/swagger-ts";
+import createSwaggerZod from "@kubb/swagger-zod";
+import createSwaggerClient from "@kubb/swagger-client";
+import createSwaggerFaker from "@kubb/swagger-faker";
+import createSwaggerMsw from "@kubb/swagger-msw";
+
+export default defineConfig({
+  root: ".",
+  input: {
+    path: "",
+  },
+  output: {
+    path: "./app/gen",
+    clean: true,
+  },
+  plugins: [
+    createSwagger({
+      output: false,
+    }),
+    createSwaggerTS({}),
+    createSwaggerZod({
+      output: {
+        path: "./zod",
+      },
+    }),
+    createSwaggerClient({
+      output: {
+        path: "./axios",
+      },
+    }),
+    createSwaggerFaker({}),
+    createSwaggerMsw({
+      output: {
+        path: "./mocks",
+      },
+    }),
+  ],
+});
---
+++ setup-test.ts
@@ -0,0 +1,19 @@
+import "@testing-library/jest-dom";
+/**
+ * Handlers are automatically generated with Kubb from Open API documentation
+ */
+// import { handlers } from "./app/gen";
+// import { setupServer } from "msw/node";
+
+window.URL.createObjectURL = vi.fn();
+
+/**
+ * Mock Service Worker can be configured once the Handlers are generated
+ */
+// const server = setupServer(...handlers);
+
+// beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+
+// afterAll(() => server.close());
+
+// afterEach(() => server.resetHandlers());
