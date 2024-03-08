67189aabd29480d8f7bd3ad7e6188ec0f3835f96
tangoyankee <miller.tim108@gmail.com>
2024-03-08 17:40:44 -0500

    create linestrings

--- app/stores/atlas.reducer.ts
+++ app/stores/atlas.reducer.ts
@@ -1,16 +1,17 @@
 import { AtlasAction, AtlasActionType } from "./atlas.actions";
 import { AtlasState } from "./atlas.context";
 import { produce } from "immer";
-import { point, lineString } from "@turf/helpers";
+import { point, lineString, multiPoint } from "@turf/helpers";
 import { getCoords } from "@turf/invariant";
-import { Feature, LineString, Point, Position } from "geojson";
+import { Feature, LineString, Point, Position, MultiPoint } from "geojson";
+import distance from "@turf/distance";
 
 export function atlasReducer(state: AtlasState, action: AtlasAction) {
   switch (action.type) {
     case AtlasActionType.UpdateMode: {
       return produce(state, (draft) => {
         const nextMode = action.payload;
-        const currentMode = draft.mode;
+        const currentMode = state.mode;
         // Clear the pen when switching modes
         draft.penFeatureCollection.features = [];
         draft.mode = currentMode === nextMode ? "select" : nextMode;
@@ -19,92 +20,97 @@ export function atlasReducer(state: AtlasState, action: AtlasAction) {
     case AtlasActionType.ClickOnMap: {
       return produce(state, (draft) => {
         const mouseCoordinate = action.payload;
-        if (draft.mode === "createPoint") {
+        const currentMode = state.mode;
+        if (currentMode === "createPoint") {
           const pt = point(mouseCoordinate) as Feature<Point, null>;
-          if (draft.shapeFeatureCollection)
-            draft.shapeFeatureCollection.features.push(pt);
+          draft.shapeFeatureCollection.features.push(pt);
           draft.mode = "select";
           return;
         }
-        if (draft.mode === "createLineString") {
-          const draftShape = draft.penFeatureCollection.features[0];
+        if (currentMode === "createLineString") {
           // Pen feature collections have two features when drawing.
-          // The first feature is a draft of the shape. Clicking on the map updates the draft.
-          // The second feature is the temporary line before the next point is selected. Moving the mouse updates the pen
+          // The first feature is a crude version of the shape, stored as a multiPoint. Clicking on the map updates the draft.
+          // The second feature is the pen - the temporary line - before the next point is selected. Moving the mouse updates the pen
+          const crudeShape = draft.penFeatureCollection.features[0];
 
-          // When there are no features yet, the draft needs to be created with a single point
-          if (draftShape === undefined) {
-            const pt = point(action.payload) as Feature<Point, null>;
-            draft.penFeatureCollection.features[0] = pt;
+          if (crudeShape === undefined) {
+            const mpt = multiPoint([action.payload]) as Feature<
+              MultiPoint,
+              null
+            >;
+            draft.penFeatureCollection.features[0] = mpt;
 
             return;
-          }
-          // When the draft geometry is a point, it needs to be turned into line string
-          if (draftShape.geometry.type === "Point") {
-            const ls = lineString([
-              draftShape.geometry.coordinates,
+          } else if (crudeShape.geometry.type === "MultiPoint") {
+            const lastCoord =
+              crudeShape.geometry.coordinates[
+                crudeShape.geometry.coordinates.length - 1
+              ];
+            const distanceClickAndLastCoord = distance(
+              lastCoord,
               mouseCoordinate,
-            ]) as Feature<LineString, null>;
-            draft.penFeatureCollection.features[0] = ls;
-          }
+              { units: "yards" },
+            );
+            if (distanceClickAndLastCoord < 10) {
+              // Cannot have a lineString with only one unique point
+              if (crudeShape.geometry.coordinates.length === 1) return;
+              // End if there is at least 1 unique point already
+              const ls = lineString(crudeShape.geometry.coordinates) as Feature<
+                LineString,
+                null
+              >;
+              draft.shapeFeatureCollection.features.push(ls);
+              draft.penFeatureCollection.features = [];
+              draft.mode = "select";
+              return;
+            }
+            // TODO: LineString support ending on keyboard enter
 
-          const penShape = draft.penFeatureCollection.features[1];
-          if (penShape === undefined) return;
-          // Mark the mouse coordinate as the next permanent point in the pen lineString
-          if (penShape.geometry.type === "LineString") {
-            const lsCoords = getCoords(penShape.geometry);
-            lsCoords.push(mouseCoordinate);
-            const ls = lineString(lsCoords) as Feature<LineString, null>;
-            draft.penFeatureCollection.features[1] = ls;
+            // Only allow up to five points
+            if (crudeShape.geometry.coordinates.length <= 4) {
+              crudeShape.geometry.coordinates.push(mouseCoordinate);
+            }
 
-            return;
+            // Check the length again. If it is 5, save the shape
+            if (crudeShape.geometry.coordinates.length === 5) {
+              const ls = lineString(crudeShape.geometry.coordinates) as Feature<
+                LineString,
+                null
+              >;
+              draft.shapeFeatureCollection.features.push(ls);
+              draft.penFeatureCollection.features = [];
+              draft.mode = "select";
+            } else if (crudeShape.geometry.coordinates.length > 5) {
+              throw new Error("Two many points in LineString");
+            }
           }
         }
       });
     }
     case AtlasActionType.HoverOverMap: {
       return produce(state, (draft) => {
-        if (draft.mode !== "createLineString") return;
+        if (state.mode !== "createLineString") return;
 
-        const draftShape = draft.penFeatureCollection.features[0];
-        const penShape = draft.penFeatureCollection.features[1];
+        const crudeShape = draft.penFeatureCollection.features[0];
         const mouseCoordinate = action.payload;
-        if (draftShape === undefined) {
+        if (crudeShape === undefined) {
           // If there is no shape, then we cannot create a pen from it
           return;
         }
-        if (penShape === undefined) {
-          // If there is no pen yet but there is a draft shape, we must create the pen
-          let penShapeFirstCoordinate: Position = [];
-          if (draftShape.geometry.type === "Point") {
-            penShapeFirstCoordinate = draftShape.geometry.coordinates;
-          } else if (draftShape.geometry.type === "LineString") {
-            penShapeFirstCoordinate =
-              draftShape.geometry.coordinates[
-                draftShape.geometry.coordinates.length - 1
-              ];
-          } else {
-            throw new Error("Invalid draft shape");
-          }
 
-          const ls = lineString([
-            penShapeFirstCoordinate,
+        // The pen consists of the points from the crude shape and a point from the mouse position
+        if (crudeShape.geometry.type === "MultiPoint") {
+          const penShape = draft.penFeatureCollection.features[1];
+          const penCoordinates = [
+            ...crudeShape.geometry.coordinates,
             mouseCoordinate,
-          ]) as Feature<LineString, null>;
-          draft.penFeatureCollection.features[1] = ls;
-
-          return;
-        }
-        // We now have defined draft and pen shapes
-        // replace the last coordinate of the pen shape with the current mouse position
-        if (penShape.geometry.type === "LineString") {
-          const lsCoords = getCoords(penShape.geometry);
-          lsCoords[lsCoords.length - 1] = mouseCoordinate;
-
-          const ls = lineString(lsCoords) as Feature<LineString, null>;
-          draft.penFeatureCollection.features[1] = ls;
-
-          return;
+          ];
+          if (penShape === undefined) {
+            const ls = lineString(penCoordinates) as Feature<LineString>;
+            draft.penFeatureCollection.features[1] = ls;
+          } else if (penShape.geometry.type === "LineString") {
+            penShape.geometry.coordinates = penCoordinates;
+          }
         }
       });
     }
